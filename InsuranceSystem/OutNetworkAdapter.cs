6d75f6c1fe7662726ffe00b9bd1b973f9707a886
Grace Hrabik <ghrabik@css.edu>
2024-10-06 22:05:07 -0500

    Made changes for assignment

---
+++ InsuranceSystem/OutNetworkAdapter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Unit5_AdapterPatternPatient_Blazor.InsuranceSystem
+{
+    public class OutNetworkAdapter : InsuranceInterface
+    {
+        OutNetworkPatient patient;
+        public OutNetworkAdapter(string newName, int newPolicyNumber) {
+            patient = new OutNetworkPatient(newName, newPolicyNumber);
+        }
+        public decimal CoverageAmount(int ProcedureID, decimal ProcedureCost)
+        {
+            decimal coveragePercent = patient.CoveragePercent(ProcedureCost);
+            return (coveragePercent*ProcedureCost);
+        }
+
+        public string getPatientName()
+        {
+            return patient.getPatientName();
+        }
+
+        public string getPolicyNumber()
+        {
+            return patient.PolicyNumber.ToString();
+        }
+
+        public bool IsCovered(string patientName, string policyNumber)
+        {
+            //string covered = patient.IsCovered(patientName, policyNumber);
+
+            // Call the IsCovered method from OutNetworkPatient with the converted policy number
+            int policyNumberInt = int.Parse(policyNumber);
+            string covered = patient.IsCovered(patientName, policyNumberInt);
+
+
+            return covered.Equals("yes", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
